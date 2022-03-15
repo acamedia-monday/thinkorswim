9218b3550706cef192d1c2f3ada48faeee55484f
Anton Ismukhambetov <101420639+acamedia-monday@users.noreply.github.com>
2022-03-15 18:57:17 +0300

    Update test0.py

--- test0.py
+++ test0.py
@@ -1,11 +1,36 @@
 import datetime as dt
+from dateutil.relativedelta import relativedelta
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy.stats import norm
 
-class Portfolio_Model():
-    def get_bond_price(my=.20):
+class Pricer():
+    opt = {'type':'call','t':dt.date(2022,6,17),'r':.01,'vol':.3}
+    bc = np.busdaycalendar(holidays=['2022-01-07','2022-02-23','2022-03-08','2022-05-02','2022-05-09','2022-11-04'])
+    spec = {'csize':1,'cval':114.213,'margin':5732.86}
+    
+    # Function returns option delta according to the Black Scholes model.
+    def get_delta(self, stock=100, strike=100):
+        next_year = dt.date.today() + relativedelta(years=1)
+        f_date = self.opt['t'] - relativedelta(days=7)
+        wdays = np.busday_count(dt.date.today().strftime('%Y-%m-%d'), next_year.strftime('%Y-%m-%d'),
+                                busdaycal=self.bc)
+        t = np.busday_count(f_date.strftime('%Y-%m-%d'), self.opt['t'].strftime('%Y-%m-%d'),
+                            busdaycal=self.bc)/wdays
+        d1 = np.log(np.array(stock)/strike)+(self.opt['r']+self.opt['vol']**2/2)*t
+        d1 = norm.cdf(d1/(self.opt['vol']*np.sqrt(t)))
+        if self.opt['type'] == 'put': d1 = d1-1
+        return d1
+    
+    def get_ladder(self, s_start=100, s_end=100):
+        ladder = pd.DataFrame(np.linspace(s_start,s_end,11),columns=['stock'])
+        ladder['delta'] = self.get_delta(ladder['stock'], s_end)
+        ladder['gamma'] = ladder['delta'] - ladder['delta'].shift(1)
+        ladder.loc[ladder['gamma'].isna(), 'gamma'] = ladder['delta']
+        return ladder
+
+        def get_bond_price(my=.20):
         bond = pd.DataFrame({'date':['2022-02-02','2022-08-03','2023-02-01','2023-08-02','2024-01-31','2024-07-31',
                                      '2025-01-29','2025-07-30','2026-01-28','2026-07-29','2027-01-27','2027-07-28',
                                      '2028-01-26','2028-07-26','2029-01-24','2029-07-25','2030-01-23','2030-07-24',
