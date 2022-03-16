e739c6c5af0397630082600fc4f1de18af499a1c
Anton Ismukhambetov <101420639+acamedia-monday@users.noreply.github.com>
2022-03-16 19:12:27 +0300

    Update test0.py

--- test0.py
+++ test0.py
@@ -6,31 +6,12 @@ import pandas as pd
 from scipy.stats import norm
 
 class Pricer():
-    opt = {'type':'call','t':dt.date(2022,6,17),'r':.01,'vol':.3}
+    opt = {'t':dt.date(2022,6,17),'r':.01,'vol':.3,'csize':1,'cval':114.213,'margin':5732.86}
+    trade = {'dir':'buy','stop':None,'pl':0.0}
     bc = np.busdaycalendar(holidays=['2022-01-07','2022-02-23','2022-03-08','2022-05-02','2022-05-09','2022-11-04'])
-    spec = {'csize':1,'cval':114.213,'margin':5732.86}
     
-    # Function returns option delta according to the Black Scholes model.
-    def get_delta(self, stock=100, strike=100):
-        next_year = dt.date.today() + relativedelta(years=1)
-        f_date = self.opt['t'] - relativedelta(days=7)
-        wdays = np.busday_count(dt.date.today().strftime('%Y-%m-%d'), next_year.strftime('%Y-%m-%d'),
-                                busdaycal=self.bc)
-        t = np.busday_count(f_date.strftime('%Y-%m-%d'), self.opt['t'].strftime('%Y-%m-%d'),
-                            busdaycal=self.bc)/wdays
-        d1 = np.log(np.array(stock)/strike)+(self.opt['r']+self.opt['vol']**2/2)*t
-        d1 = norm.cdf(d1/(self.opt['vol']*np.sqrt(t)))
-        if self.opt['type'] == 'put': d1 = d1-1
-        return d1
-    
-    def get_ladder(self, s_start=100, s_end=100):
-        ladder = pd.DataFrame(np.linspace(s_start,s_end,11),columns=['stock'])
-        ladder['delta'] = self.get_delta(ladder['stock'], s_end)
-        ladder['gamma'] = ladder['delta'] - ladder['delta'].shift(1)
-        ladder.loc[ladder['gamma'].isna(), 'gamma'] = ladder['delta']
-        return ladder
-
-        def get_bond_price(my=.20):
+    # Function returns both dirty and clean prices of the OFZ 26233 issue.
+    def get_bond_price(my=.20):
         bond = pd.DataFrame({'date':['2022-02-02','2022-08-03','2023-02-01','2023-08-02','2024-01-31','2024-07-31',
                                      '2025-01-29','2025-07-30','2026-01-28','2026-07-29','2027-01-27','2027-07-28',
                                      '2028-01-26','2028-07-26','2029-01-24','2029-07-25','2030-01-23','2030-07-24',
@@ -51,3 +32,40 @@ class Pricer():
         bond['discounted'] = bond['flow']*np.power(1+my,-bond['date'])
         print('Gross bond price is {gross}'.format(gross=bond['discounted'].sum()))
         print('Clean bond price is {clean}'.format(clean=bond['discounted'].sum()-accrued))
+
+    # Function returns option delta according to the Black Scholes model.
+    def get_delta(self, stock=100, strike=120):
+        next_year = dt.date.today() + relativedelta(years=1)
+        f_date = self.opt['t'] - relativedelta(days=7)
+        wdays = np.busday_count(dt.date.today().strftime('%Y-%m-%d'), next_year.strftime('%Y-%m-%d'),
+                                busdaycal=self.bc)
+        t = np.busday_count(f_date.strftime('%Y-%m-%d'), self.opt['t'].strftime('%Y-%m-%d'),
+                            busdaycal=self.bc)/wdays
+        d1 = np.log(np.array(stock)/strike)+(self.opt['r']+self.opt['vol']**2/2)*t
+        return norm.cdf(d1/(self.opt['vol']*np.sqrt(t)))
+    
+    # Function returns the ladder with gamma-sized position entries.
+    def get_ladder(self, s_start=100, s_end=120, size=None):
+        multi = 1
+        pl = 1.0
+        epsi = 0.0
+        ladder = pd.DataFrame(np.linspace(s_start,s_end,11),columns=['stock'])
+        ladder['delta'] = self.get_delta(ladder['stock'], s_end)
+        if s_end>s_start:
+            if self.trade['dir'] == 'sell': ladder['delta'] = -ladder['delta']
+        else:
+            ladder['delta'] = ladder['delta'] - 1
+            if self.trade['dir'] == 'sell': ladder['delta'] = -ladder['delta']
+        ladder['gamma'] = ladder['delta'] - ladder['delta'].shift(1)
+        ladder.loc[ladder['gamma'].isna(), 'gamma'] = ladder['delta']
+
+        ladder['multi'] = multi
+        if size is not None and len(size)==11: ladder['size'] = size
+        else: ladder['size'] = ladder['gamma']*ladder['multi']
+        ladder['val'] = ladder['stock']*ladder['size']*self.opt['csize']*self.opt['cval']
+        ladder['val'] = ladder['val'].round(decimals=5)
+        ladder['cumval'] = ladder['val'].cumsum(skipna=True)
+        ladder['cumval'] = ladder['cumval'].round(decimals=5)
+        ladder['avg'] = ladder['cumval']/(ladder['delta']*ladder['multi']*self.opt['csize']*self.opt['cval'])
+            
+        return ladder
