c5817d6f0ae5473f46ebaae6bcc661a4e8a2f788
Anton Ismukhambetov <101420639+acamedia-monday@users.noreply.github.com>
2022-03-17 18:35:16 +0300

    Update test0.py

--- test0.py
+++ test0.py
@@ -7,8 +7,10 @@ from scipy.stats import norm
 
 class Pricer():
     opt = {'t':dt.date(2022,6,17),'r':.01,'vol':.3,'csize':1,'cval':114.213,'margin':5732.86}
-    trade = {'dir':'buy','stop':None,'pl':0.0}
+    trade = {'dir':'buy','stop':20,'pl':0.0}
     bc = np.busdaycalendar(holidays=['2022-01-07','2022-02-23','2022-03-08','2022-05-02','2022-05-09','2022-11-04'])
+    size = None
+    ladder = None
     
     # Function returns both dirty and clean prices of the OFZ 26233 issue.
     def get_bond_price(my=.20):
@@ -33,39 +35,43 @@ class Pricer():
         print('Gross bond price is {gross}'.format(gross=bond['discounted'].sum()))
         print('Clean bond price is {clean}'.format(clean=bond['discounted'].sum()-accrued))
 
-    # Function returns option delta according to the Black Scholes model.
-    def get_delta(self, stock=100, strike=120):
+    # Function returns option deltas and gammas according to the Black Scholes model.
+    def get_greeks(self, stock=100, strike=120):
+        if self.size is None:
+            item = 11
+            self.ladder = pd.DataFrame([0.0]*item, columns=['size'])
+        else:
+            item = len(self.size)
+            self.ladder = pd.DataFrame(self.size, columns=['size'])
+        self.ladder['stock'] = np.linspace(stock,strike,item)
         next_year = dt.date.today() + relativedelta(years=1)
         f_date = self.opt['t'] - relativedelta(days=7)
         wdays = np.busday_count(dt.date.today().strftime('%Y-%m-%d'), next_year.strftime('%Y-%m-%d'),
                                 busdaycal=self.bc)
         t = np.busday_count(f_date.strftime('%Y-%m-%d'), self.opt['t'].strftime('%Y-%m-%d'),
                             busdaycal=self.bc)/wdays
-        d1 = np.log(np.array(stock)/strike)+(self.opt['r']+self.opt['vol']**2/2)*t
-        return norm.cdf(d1/(self.opt['vol']*np.sqrt(t)))
+        d1 = np.log(np.array(stock)/stock[-1])+(self.opt['r']+self.opt['vol']**2/2)*t
+        self.ladder['delta'] = norm.cdf(d1/(self.opt['vol']*np.sqrt(t)))
+        if strike>stock:
+            if self.trade['dir'] == 'sell': self.ladder['delta'] = -self.ladder['delta']
+        else:
+            self.ladder['delta'] = self.ladder['delta'] - 1
+            if self.trade['dir'] == 'sell': self.ladder['delta'] = -self.ladder['delta']
+        self.ladder['gamma'] = self.ladder['delta'] - self.ladder['delta'].shift(1)
+        self.ladder.loc[self.ladder['gamma'].isna(), 'gamma'] = self.ladder['delta']
     
     # Function returns the ladder with gamma-sized position entries.
-    def get_ladder(self, s_start=100, s_end=120, size=None):
-        multi = 1
-        pl = 1.0
-        epsi = 0.0
-        ladder = pd.DataFrame(np.linspace(s_start,s_end,11),columns=['stock'])
-        ladder['delta'] = self.get_delta(ladder['stock'], s_end)
-        if s_end>s_start:
-            if self.trade['dir'] == 'sell': ladder['delta'] = -ladder['delta']
-        else:
-            ladder['delta'] = ladder['delta'] - 1
-            if self.trade['dir'] == 'sell': ladder['delta'] = -ladder['delta']
-        ladder['gamma'] = ladder['delta'] - ladder['delta'].shift(1)
-        ladder.loc[ladder['gamma'].isna(), 'gamma'] = ladder['delta']
-
-        ladder['multi'] = multi
-        if size is not None and len(size)==11: ladder['size'] = size
-        else: ladder['size'] = ladder['gamma']*ladder['multi']
-        ladder['val'] = ladder['stock']*ladder['size']*self.opt['csize']*self.opt['cval']
-        ladder['val'] = ladder['val'].round(decimals=5)
-        ladder['cumval'] = ladder['val'].cumsum(skipna=True)
-        ladder['cumval'] = ladder['cumval'].round(decimals=5)
-        ladder['avg'] = ladder['cumval']/(ladder['delta']*ladder['multi']*self.opt['csize']*self.opt['cval'])
-            
-        return ladder
+    def get_ladder(self, multi=1):
+        self.ladder['multi'] = multi
+        if self.size is None: self.ladder['size'] = self.ladder['gamma']*self.ladder['multi']
+        self.ladder['cumsize'] = self.ladder['size'].cumsum(skipna=True)
+        self.ladder['val'] = self.ladder['stock']*self.ladder['size']*self.opt['csize']*self.opt['cval']
+        self.ladder['val'] = self.ladder['val'].round(decimals=5)
+        self.ladder['cumval'] = self.ladder['val'].cumsum(skipna=True)
+        self.ladder['cumval'] = self.ladder['cumval'].round(decimals=5)
+        self.ladder['avg'] = self.ladder['cumval']/(self.ladder['delta']*self.ladder['multi']*self.opt['csize']*self.opt['cval'])
+        self.ladder['stop'] = self.ladder['stock']-self.trade['stop']
+        self.ladder.loc[self.ladder['delta']<0,'stop'] = self.ladder['stock']+self.trade['stop']
+        self.ladder['loss'] = self.ladder['stop']*self.ladder['cumsize']*self.opt['csize']*self.opt['cval']
+        self.ladder['loss'] = self.ladder['loss'].round(decimals=5)
+        self.ladder['loss'] = self.laddder['loss']-self.ladder['cumval']
